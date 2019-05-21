49768de8128834bd21407299f97621bbdb9814a1
Vadim Rupets <vadim.rupets@yandex.by>
2019-05-21 19:17:06 +0300

    Created Xcode project

---
+++ JobsList/JobsList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JobsList
+//
+//  Created by Vadim Rupets on 5/21/19.
+//  Copyright © 2019 Vadim Rupets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
